fffef52a5a74b9430de4abe35f806ea23bbe3ad7
soswald <oswaldbstephen@gmail.com>
2026-07-15 17:17:51 +0100

    Recent changes

---
+++ learnings/lists_and_indexing.py
@@ -0,0 +1,5 @@
+fruits = ['Apple', 'Guava', 'Pear'] #this form gives a list of things stored within a string. Each component within the string is given an indexed number.
+print (fruits[2]) #Machine counts from 0, so any number within the square brackets will be what will be printed.
+
+fruits.append ('Strawberry') #the append function adds to the initial section
+print (fruits)
