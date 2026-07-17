9f6fffc4c1fe9f11971ff10ef87e2c2401854902
soswald <oswaldbstephen@gmail.com>
2026-07-17 17:20:25 +0100

    Additions

--- learnings/lists_and_indexing.py
+++ learnings/lists_and_indexing.py
@@ -1,5 +1,11 @@
 fruits = ['Apple', 'Guava', 'Pear'] #this form gives a list of things stored within a string. Each component within the string is given an indexed number.
 print (fruits[2]) #Machine counts from 0, so any number within the square brackets will be what will be printed.
 
-fruits.append ('Strawberry') #the append function adds to the initial section
+fruits.append ('Strawberry') #the append function adds to the initial list. so name.append allows for a new element to be added to the list.
 print (fruits)
+
+fruits[1] = 'Orange' #This command changes the component in the initial list to the newly appointed information given, i.e, in this case, Orange.
+print (fruits)
+
+#However, outside lists and indexing, there exist another datatype similar in function to the list function, known as the tuple. It is used to identify positions, colours and the likes, and it is written like this;
+
